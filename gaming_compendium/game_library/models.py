82509ae0a2ff2bf961abcba6b34068ef09309354
Clovo <72047946+Clovotc@users.noreply.github.com>
2024-04-17 14:23:56 -0500

    Created the game model

--- gaming_compendium/game_library/models.py
+++ gaming_compendium/game_library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    game_name = models.CharField(max_length = 100, primary_key = True)
+    game_description = models.TextField()
+    game_genre = models.CharField(max_length = 50)
+    game_rating = models.CharField(max_length = 50)
+    game_completed = models.BooleanField()
+    time_to_beat = models.TextField()
+
+    def __str__(self):
+        return self.game_name
