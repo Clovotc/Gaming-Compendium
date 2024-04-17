f52ce541fcb13afe0e037af35fb150c78f578056
Clovo <72047946+Clovotc@users.noreply.github.com>
2024-04-17 14:12:14 -0500

    Created random game selector app

--- gaming_compendium/gaming_compendium/settings.py
+++ gaming_compendium/gaming_compendium/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'game_library',
     'login_controls',
+    'random_game_selector',
 ]
 
 MIDDLEWARE = [
---
+++ gaming_compendium/random_game_selector/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RandomGameSelectorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'random_game_selector'
