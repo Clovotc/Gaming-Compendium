2caf53bb86f1af122592aa53beca8f37245e4e28
Clovo <72047946+Clovotc@users.noreply.github.com>
2024-04-17 15:34:46 -0500

    Added the genre, game, and developer models. Created templates folder along with base.html and game_card.html

--- gaming_compendium/game_library/admin.py
+++ gaming_compendium/game_library/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from game_library.models import Game, Genre, Developer
 
 # Register your models here.
+
+admin.site.register(Game)
+admin.site.register(Genre)
+admin.site.register(Developer)
