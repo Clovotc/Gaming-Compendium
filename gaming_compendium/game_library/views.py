2ea542d494e62b0fae4fb8e07a6daff50d24efbe
Clovo <72047946+Clovotc@users.noreply.github.com>
2024-04-18 15:52:40 -0500

    Created collapsable cards to display the games in the database. Created the navbar for the base.html

--- gaming_compendium/game_library/views.py
+++ gaming_compendium/game_library/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from game_library.models import Game
 
 # Create your views here.
+
+def index(request):
+    game_list = Game.objects.all()
+    return render(request, 'game_library/index.html', {'game_list': game_list})
