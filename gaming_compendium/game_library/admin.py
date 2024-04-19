ad74826d7f148127905f5815777959a3e6fd5807
Clovo <72047946+Clovotc@users.noreply.github.com>
2024-04-19 14:49:42 -0500

    Added some more fields to the game model. Created the platform model. Fixed the accordian error. Added more games to the database.

--- gaming_compendium/game_library/admin.py
+++ gaming_compendium/game_library/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from game_library.models import Game, Genre, Developer
+from game_library.models import Game, Genre, Developer, Platform
 
 # Register your models here.
 
 admin.site.register(Game)
 admin.site.register(Genre)
 admin.site.register(Developer)
+admin.site.register(Platform)
--- gaming_compendium/game_library/models.py
+++ gaming_compendium/game_library/models.py
@@ -20,12 +20,35 @@ class Genre(models.Model):
         ]
 
 
+class Platform(models.Model):
+    name = models.CharField(max_length = 50, unique = True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='platform_name_case_insensitive_unique',
+                violation_error_message = "Platform already exists (case insensitive match)"
+            ),
+        ]
+
+
 class Game(models.Model):
+    class Players(models.TextChoices):
+        single = "Singleplayer", "Singleplayer"
+        multiple = "Multiplayer", "Multiplayer"
+        both = "Both", "Both"
+
     title = models.CharField(max_length = 100, primary_key = True)
     description = models.TextField()
     genre = models.ManyToManyField(Genre)
+    platform = models.ManyToManyField(Platform)
     developer = models.ForeignKey('Developer', on_delete=models.SET_NULL, null=True)
     rating = models.DecimalField(max_digits = 10, decimal_places = 2, default = 0)
+    player = models.CharField(max_length = 20, choices = Players.choices, default = Players.single)
     is_complete = models.BooleanField()
     time_to_beat = models.TextField()
 
