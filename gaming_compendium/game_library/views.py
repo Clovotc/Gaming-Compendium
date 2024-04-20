5a2a97e50dc53f00606a1b35ae117ce4d92c5b44
Clovo <72047946+Clovotc@users.noreply.github.com>
2024-04-20 17:56:04 -0500

    Created filters for the game list and the search bar now works

--- gaming_compendium/game_library/views.py
+++ gaming_compendium/game_library/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
-from game_library.models import Game
+from game_library.models import Game, Genre, Developer, Platform
 
 # Create your views here.
 
+def is_valid_query_param(param):
+    return param != "" and param is not None
+
+
 def index(request):
-    game_list = Game.objects.all()
-    return render(request, 'game_library/index.html', {'game_list': game_list})
+    game_list = Game.objects.all().order_by("title")
+    genre_list = Genre.objects.all().order_by("name")
+    developer_list = Developer.objects.all().order_by("name")
+    platform_list = Platform.objects.all().order_by("name")
+
+    title_contains_search = request.GET.get("title_contains")
+    selected_genre = request.GET.get("genre")
+    selected_developer = request.GET.get("developer")
+    selected_platform = request.GET.get("platform")
+
+    if  is_valid_query_param(title_contains_search):
+        game_list = game_list.filter(title__icontains = title_contains_search)
+
+    if is_valid_query_param(selected_genre) and selected_genre != 'Choose...':
+        game_list = game_list.filter(genre__name = selected_genre)
+
+    if is_valid_query_param(selected_developer) and selected_developer != 'Choose...':
+        game_list = game_list.filter(developer__name = selected_developer)
+
+    if is_valid_query_param(selected_platform) and selected_platform != 'Choose...':
+        game_list = game_list.filter(platform__name = selected_platform)
+
+    context = {"game_list": game_list, 
+               "genre_list": genre_list, 
+               "developer_list": developer_list, 
+               "platform_list": platform_list,}
+    
+    return render(request, "game_library/index.html", context)
