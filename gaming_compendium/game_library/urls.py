9770c720d7102e099712e701ee1c204f696aa2ab
Legolas151 <160072632+Legolas151@users.noreply.github.com>
2024-05-01 18:40:34 -0500

    Random Game views created

--- gaming_compendium/game_library/urls.py
+++ gaming_compendium/game_library/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from game_library import views
+from random_game_selector import views as random_views
 
 urlpatterns = [
     path('', views.index, name='games'),
+    path('random/', random_views.RandomGameView, name = 'randomgame'),
+    path('random/randomselected/', random_views.RandomGameSelectedView, name = 'randomgameselected'),
 ]
--- gaming_compendium/random_game_selector/views.py
+++ gaming_compendium/random_game_selector/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def RandomGameView(request):
+    
+    return render(request, "random_game_selector/RandomGameView.html")
+
+def RandomGameSelectedView(request):
+    
+    return render(request, "random_game_selector/RandomGameSelectedView.html")
