e1561e1731b612860a27a769a152769e5233b374
Legolas151 <160072632+Legolas151@users.noreply.github.com>
2024-05-02 14:27:31 -0500

    random game selector functional

--- gaming_compendium/random_game_selector/views.py
+++ gaming_compendium/random_game_selector/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from game_library.models import Game
-
+import random
 # Create your views here.
 
 def RandomGameView(request):
@@ -8,9 +8,20 @@ def RandomGameView(request):
     return render(request, "random_game_selector/RandomGameView.html")
 
 def RandomGameSelectedView(request):
-    
-    # random = Game.title
 
-    # random_game = f"/static/random_game_selector/images/{random}.jpg"
 
-    return render(request, "random_game_selector/RandomGameSelectedView.html")
+    ran = random.randint(1,50)
+    random_game = Game.objects.filter(number=ran).values_list('title', flat = True)
+    random_game_string = str(random_game)
+    random_game_string = random_game_string[12:-3]
+    formatted_random_game_string=random_game_string
+    random_game_image = "/static/random_game_selector/images/{}.jpg".format(formatted_random_game_string) 
+    random_game_image_url = random_game_image.replace(" ", "_")
+
+    context = {
+        "formatted_random_game_string": formatted_random_game_string,
+        "random_game_image_url": random_game_image_url,
+    }
+
+    return render(request, "random_game_selector/RandomGameSelectedView.html", context)
+ 
